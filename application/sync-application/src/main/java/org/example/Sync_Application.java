65b7289997fa232de8f56c84a8f5480a8f623dd9
trsoo <trsoo24@naver.com>
2024-05-05 14:43:20 +0900

    Feat : application-sync 모듈 생성

---
+++ application/sync-application/src/main/java/org/example/Sync_Application.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sync_Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Sync_Application.class, args);
+    }
+}
